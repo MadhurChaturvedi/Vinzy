c3bed13e699e1c91cde169a962f2c0c2e3da1577
Madhur Chaturvedi <madhurchaturvedi2000@gmail.com>
2025-01-17 00:07:53 +0530

    fix the recommended button bug +

--- src/Recommended/Recommended.js
+++ src/Recommended/Recommended.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import "./Recommended.css"
 import Button from '../components/Button'
-export default function Recommended({handleClick}) {
+export default function Recommended({handleChange}) {
   return <>
   <div>
     <h2 className='recommended-title'>Recommende</h2>
     <div className='recommended-flex'>
-       <Button OnClickHandler={handleClick} value="" title="All" />
-       <Button OnClickHandler={handleClick} value="Nike" title="Nike" />
-       <Button OnClickHandler={handleClick} value="Adidas" title="Adidas"  />
-       <Button OnClickHandler={handleClick} value="Puma" title="Puma" />
-       <Button OnClickHandler={handleClick} value="Vans" title="Vans" />
+       <Button OnClickHandler={handleChange} value="" title="All" />
+       <Button OnClickHandler={handleChange} value="Nike" title="Nike" />
+       <Button OnClickHandler={handleChange} value="Adidas" title="Adidas"  />
+       <Button OnClickHandler={handleChange} value="Puma" title="Puma" />
+       <Button OnClickHandler={handleChange} value="Vans" title="Vans" />
     </div>
   </div>
   </>
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
-export default function Button({OnClickHandler,value,title}) {
+export default function Button({ OnClickHandler, value, title }) {
     return <button onClick={OnClickHandler} value={value} className='btns'>
         {title}
-        </button>
+    </button>
 }
