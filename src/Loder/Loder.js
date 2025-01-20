a3bc323e36898ddbfb92eca018e5da919fe351b1
Madhur Chaturvedi <madhurchaturvedi2000@gmail.com>
2025-01-20 23:00:28 +0530

    chage loader

--- src/Loder/Loder.js
+++ src/Loder/Loder.js
@@ -1,14 +1,10 @@
 import React from 'react'
-import "./Loder.css"
+import { Atom } from "react-loading-indicators";
 export default function Loder() {
   return <div className="pyramid-loader">
-  <div className="wrapper">
-    <span className="side side1"></span>
-    <span className="side side2"></span>
-    <span className="side side3"></span>
-    <span className="side side4"></span>
-    <span className="shadow"></span>
+    <div className="wrapper">
+      <Atom color="#7428be" size="large" text="" textColor="" />
+    </div>
   </div>
-</div>
 
 }
